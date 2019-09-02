1c0cca043534f18fad5f141f4cbe345f485f14cd
anshtyagi23 <tyagiansh23@gmail.com>
2019-09-02 22:30:29 +0530

    Add files via upload

---
+++ ques1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//Ansh Tyagi IT 11912079
+int main(){
+	int l,b,a;
+	printf("Enter the length of Rectangle:\n");
+	scanf("%d",&l);
+	printf("Enter the width of Rectangle:\n");
+	scanf("%d",&b);
+	a=l*b;
+	printf("Area: %d\n",a );
+	return 0;
+
+
+}
